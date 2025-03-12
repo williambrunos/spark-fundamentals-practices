c09e106b9be4db3b17b235ca6bd13a81c2874837
William <williambrunosalesdepaulalima@gmail.com>
2025-03-12 16:03:32 -0300

    feat: adicionando comandos de pyspark pandas

--- pandas_on_spark/app/pandas_on_spark.py
+++ pandas_on_spark/app/pandas_on_spark.py
@@ -5,6 +5,10 @@ spark = SparkSession.builder.appName("pandas_on_spark_app") \
     .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
     .getOrCreate()
 
-print(spark)
+business_data = ps.read_json("data/business.json", lines=True)
+reviews_data = ps.read_json("data/reviews.json", lines=True)
+users_data = ps.read_json("data/users.json", lines=True)
+
+print(f"\n\nusers data: \n\n{users_data.describe()}\n\n")
 
 spark.stop()
